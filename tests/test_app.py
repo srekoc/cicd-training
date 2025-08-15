2939d887d00794c63723857062e9fce7a8d2d18e
Sreekanth Kocharlakota <sreekanth.kocharlakota@gmail.com>
2025-08-15 02:04:11 -0700

    Checking in pytest test code

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b 'Hello World' in response.data
+
+def test_greet():
+    client = app.test_client()
+    response = client.get("/greet/Sree")
+    assert response.code == 200
+    assert b 'Hello, Sree' in response.data
+
