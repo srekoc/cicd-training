11246e15e5d181aab225ce6d2e478783b4def563
Sreekanth Kocharlakota <sreekanth.kocharlakota@gmail.com>
2025-08-15 21:30:32 -0700

    Added John's code

---
+++ app-john-goodbye.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import socket
+
+app = Flask (__name__)
+
+@app.route ('/goodbye')
+def hello():
+    return f'Goodbye @{ip_address}!!'
+
+@app.route ('/')
+def home():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return f'Hello World @{ip_address}'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
