7f4f3dde6da1a0ab37c8340c64ae3894e87e92e9
Sreekanth Kocharlakota <sreekanth.kocharlakota@gmail.com>
2025-08-15 21:23:16 -0700

    added test cases for Mark's code

--- tests/test_app_main.py
+++ tests/test_app_main.py
@@ -11,3 +11,9 @@ def test_home():
     response = client.get('/')
     assert response.status_code == 200
     assert b'Hello World' in response.data
+
+def test_hello():
+    client = app.test_client()
+    response = client.get('/hello')
+    assert response.status_code == 200
+    assert b'Hello Mark' in response.data
