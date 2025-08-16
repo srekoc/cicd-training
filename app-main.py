09fbb8f411e201dd271c731d776544a67877d74e
Sreekanth Kocharlakota <sreekanth.kocharlakota@gmail.com>
2025-08-15 19:06:52 -0700

    Checking main branch code

---
+++ app-main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import socket
+
+app = Flask (__name__)
+
+@app.route ('/')
+def home():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return f'Hello World @{ip_address}'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
