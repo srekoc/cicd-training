4d43aef014e5cf298ec1a73e28cdda4fe343e9f4
Sreekanth Kocharlakota <sreekanth.kocharlakota@gmail.com>
2025-08-15 19:27:20 -0700

    Fixed syntax errors in test cases

--- tests/test_app_main.py
+++ tests/test_app_main.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b 'Hello World' in response.data
+    assert b'Hello World' in response.data
