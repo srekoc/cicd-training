142a605ba3c980230780e4f8b48235f05dda7d90
root <root@REM-LT0591.localdomain>
2025-08-16 15:40:05 -0700

    my first commit

---
+++ tests/test_app_main.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+# Add parent directory (project root) to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello World' in response.data
